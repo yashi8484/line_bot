c6998b6e761d9c250cba797c9b419cb49bc10fad
yashi8484 <kobayashi@giftee.co>
2018-05-31 12:11:50 +0900

    [change/add] botのAI実装追加、正解時に連続回答できるように変更

--- domemo.js
+++ domemo.js
@@ -10,7 +10,9 @@ module.exports = class Domemo {
     this.bot_tiles = []
     this.player_tiles = []
     this.field_tiles = []
+    this.exclude_tiles = []
     this.started = false
+    this.winner = null
   }
 
   // ゲームスタート
@@ -22,16 +24,19 @@ module.exports = class Domemo {
       }
     }
     // console.log(this.all_tiles)
+    // 全tileを除外tileリストに一旦コピー
+    // このあと各プレイヤーの手札を配り、余った分が除外分となる
+    this.exclude_tiles = this.all_tiles.slice()
 
     // botのtileを選択する
     this.bot_tiles = this.distributeTiles(HAND_TILE_COUNT)
     this.bot_tiles.sort((x, y) => x - y)
-    // console.log(this.bot_tiles)
+    // console.log(`bot:${this.bot_tiles}`)
 
     // プレイヤーのtileを選択する
     this.player_tiles = this.distributeTiles(HAND_TILE_COUNT)
     this.player_tiles.sort((x, y) => x - y)
-    // console.log(this.player_tiles)
+    // console.log(`player:${this.player_tiles}`)
 
     // 場のtileを選択する
     this.field_tiles = this.distributeTiles(FIELD_TILE_COUNT)
@@ -39,7 +44,7 @@ module.exports = class Domemo {
     // console.log(this.field_tiles)
 
     // 余り=除外
-    // console.log(this.all_tiles)
+    // console.log(this.exclude_tiles)
 
     this.started = true
   }
@@ -47,19 +52,24 @@ module.exports = class Domemo {
   // 札を配る
   distributeTiles(tile_count) {
     let tiles = []
-    if (this.all_tiles.length < tile_count) {
+    if (this.exclude_tiles.length < tile_count) {
       // tile残り枚数が手札枚数以下:配れない
       return tiles
     }
     while (tiles.length < tile_count) {
       let selected_num = Math.floor(Math.random() * MAX_TILE_NUMBER) + 1
-      let num_tile_index = this.all_tiles.indexOf(selected_num)
+      let num_tile_index = this.exclude_tiles.indexOf(selected_num)
       if (num_tile_index >= 0) {
-        tiles.push(this.all_tiles[num_tile_index])
-        this.all_tiles.splice(num_tile_index, 1)
+        tiles.push(this.exclude_tiles[num_tile_index])
+        this.exclude_tiles.splice(num_tile_index, 1)
       }
     }
     return tiles
+  }
+
+  // 全tileのリストを公開する
+  openAllTiles() {
+    return this.all_tiles
   }
 
   // botのtileを公開する
@@ -67,9 +77,19 @@ module.exports = class Domemo {
     return this.bot_tiles
   }
 
+  // botのtile枚数を公開する
+  openBotTilesCount() {
+    return this.bot_tiles.length
+  }
+
   // プレイヤーのtile枚数を公開する
   openPlayerTilesCount() {
     return this.player_tiles.length
+  }
+
+  // プレイヤーのtileを公開する
+  openPlayerTiles() {
+    return this.player_tiles
   }
 
   // 場のtileを公開する
@@ -92,14 +112,38 @@ module.exports = class Domemo {
     return num
   }
 
-  // プレイヤーに回答権があるかを判定する
+  // botのtileから取り除く
+  // 取り除けた場合は、その数字を返す
+  // 取り除いたtileは、場札に移る
+  // 取り除けなかった場合は、-1を返す
+  removeFromBotTiles(num) {
+    let num_tile_index = this.bot_tiles.indexOf(num)
+    if (num_tile_index < 0) {
+      return num_tile_index
+    }
+    this.field_tiles.push(this.bot_tiles[num_tile_index])
+    this.bot_tiles.splice(num_tile_index, 1)
+    this.field_tiles.sort((x, y) => x - y)
+    return num
+  }
 
   // ゲーム終了か判定する
   isGameEnd() {
     if (this.player_tiles.length <= 0) {
       this.started = false
+      this.winner = 'player'
+      return true
+    }
+    if (this.bot_tiles.length <= 0) {
+      this.started = false
+      this.winner = 'bot'
       return true
     }
     return false
+  }
+
+  // 結果を取得する
+  getGameResult() {
+    return `${this.winner}の勝ちです`
   }
 }
--- server.js
+++ server.js
@@ -22,25 +22,31 @@ app.post('/webhook', line.middleware(config), (req, res) => {
 
 const client = new line.Client(config);
 let domemo = new Domemo();
-let answer_count = 0;
+let turn_count = 0;
 
 function handleEvent(event) {
   if (event.type !== 'message' || event.message.type !== 'text') {
     return Promise.resolve(null);
   }
+
   let reply_messages = []
 
   if (!domemo.started) {
-    if (event.message.text === 'スタート' || event.message.text === 'start') {
-      answer_count = 0;
-      domemo.start();
-      reply_messages.push(
-        'ゲームスタート！',
-        `私の手札は${domemo.openBotTiles().map(n => `[${convertToTwoBytesChar(String(n))}]`).join(' ')}`,
-        `場の札は${domemo.openFieldTiles().map(n => `[${convertToTwoBytesChar(String(n))}]`).join(' ')}`,
-        `あなたの手札は残り${domemo.openPlayerTilesCount()}枚です`,
-        '数字を1つ宣言してください'
-      )
+    switch(event.message.text) {
+      case 'スタート':
+      case 'start':
+      case 'すたーと':
+        turn_count = 0;
+        domemo.start();
+        initBotAI();
+        reply_messages.push(
+          'ゲームスタート！',
+          `botの手札は${domemo.openBotTiles().map(n => `[${convertToTwoBytesChar(String(n))}]`).join(' ')}`,
+          `場の札は${domemo.openFieldTiles().map(n => `[${convertToTwoBytesChar(String(n))}]`).join(' ')}`,
+          `playerの手札は残り${domemo.openPlayerTilesCount()}枚です`,
+          '数字を1つ宣言してください'
+        )
+        break
     }
   }
   else {
@@ -50,9 +56,9 @@ function handleEvent(event) {
       case 'カクニン':
         // 状況確認
         reply_messages.push(
-          `私の手札は${domemo.openBotTiles().map(n => `[${convertToTwoBytesChar(String(n))}]`).join(' ')}`,
+          `botの手札は${domemo.openBotTiles().map(n => `[${convertToTwoBytesChar(String(n))}]`).join(' ')}`,
           `場の札は${domemo.openFieldTiles().map(n => `[${convertToTwoBytesChar(String(n))}]`).join(' ')}`,
-          `あなたの手札は残り${domemo.openPlayerTilesCount()}枚です`,
+          `playerの手札は残り${domemo.openPlayerTilesCount()}枚です`,
         )
         break
       default:
@@ -61,11 +67,12 @@ function handleEvent(event) {
         if (event.message.text.length !== 1 || !isFinite(convertToOneBytesChar(event.message.text))) {
           return Promise.resolve(null);
         }
-        answer_count++
+        let is_correct_answer = false
 
         // 宣言した数をプレイヤーのtileから1つ取り除く
         if (domemo.removeFromPlayerTiles(+convertToOneBytesChar(event.message.text)) > 0) {
           // 取り除けた場合
+          is_correct_answer = true
           reply_messages.push(`正解！`)
         }
         else {
@@ -74,8 +81,18 @@ function handleEvent(event) {
         }
         reply_messages.push(`あなたの手札は残り${domemo.openPlayerTilesCount()}枚です`)
 
+        // 正解したら連続でもう一回
+        if (!domemo.isGameEnd() && is_correct_answer) {
+          reply_messages.push(`正解したのでもう1つ数字を宣言できます`)
+        }
+        else if (!is_correct_answer) {
+          // botの手番を行う
+          reply_messages.push(playBotTurn())
+        }
+        turn_count++
+
         if (domemo.isGameEnd()) {
-          reply_messages.push(`ゲーム終了！回答回数：${answer_count}回`)
+          reply_messages.push(`ゲーム終了！${turn_count}ターンで${domemo.getGameResult()}`)
         }
         break
     }
@@ -109,6 +126,56 @@ function convertToOneBytesChar(str) {
   return str.replace(/[０-９Ａ-Ｚａ-ｚ]/g, (c) => {
     return String.fromCharCode(c.charCodeAt(0) - 0xFEE0)
   })
+}
+
+let bot_ai_candidate_tiles
+
+// botのAI初期化
+// 全tile - プレイヤーのtile - 場のtile = AIが選択するtileの候補
+function initBotAI() {
+  bot_ai_candidate_tiles = domemo.openAllTiles()
+  let player_tiles = domemo.openPlayerTiles()
+  let field_tiles = domemo.openFieldTiles()
+  player_tiles.forEach((num) => {
+    bot_ai_candidate_tiles.splice(bot_ai_candidate_tiles.indexOf(num), 1)
+  })
+  field_tiles.forEach((num) => {
+    bot_ai_candidate_tiles.splice(bot_ai_candidate_tiles.indexOf(num), 1)
+  })
+}
+
+// botの順番を一回行う
+// 1. 選択候補の中からランダムで1つ選ぶ
+// 2. 選んだ数を選択候補から除く
+function playBotTurn() {
+  let bot_play_log_message = ''
+  let is_correct_answer = false
+  let select_num_index = Math.floor(Math.random() * bot_ai_candidate_tiles.length)
+
+  bot_play_log_message += 'botのターンを行います...'
+
+  let select_num = bot_ai_candidate_tiles[select_num_index]
+  bot_play_log_message += `\n数字を宣言：${convertToTwoBytesChar(String(select_num))}`
+
+  if (domemo.removeFromBotTiles(bot_ai_candidate_tiles[select_num_index]) > 0) {
+    // 取り除けた場合
+    is_correct_answer = true
+    bot_play_log_message += '\n正解！'
+  }
+  else {
+    // 取り除けなかった場合
+    bot_play_log_message += '\n不正解'
+  }
+  bot_ai_candidate_tiles.splice(select_num_index, 1)
+  bot_play_log_message += `botの手札は残り${domemo.openBotTilesCount()}枚です`
+
+  // 正解したら連続でもう一回
+  if (!domemo.isGameEnd() && is_correct_answer) {
+    bot_play_log_message += `\n正解したのでもう一回`
+    bot_play_log_message += playBotTurn()
+  }
+
+  return bot_play_log_message
 }
 
 app.listen(PORT);
